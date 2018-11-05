786b0a5e91d7a955d49a0ea320a9af071be66a83
Oskar Persson <oskar.persson@polken.se>
2018-11-05 11:42:06 +0100

    SQL Server from Docker

--- config/settings.py
+++ config/settings.py
@@ -75,8 +75,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'sql_server.pyodbc',
+        'NAME': 'core',
+        'USER': 'sa',
+        'PASSWORD': 'Password123?',
+        'OPTIONS': {
+            'isolation_level': 'read committed',
+        }
     }
 }
 
